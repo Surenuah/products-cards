ca7949e48a41af52c2cd5e6293202bf88858dddf
aarstanbekov <aarstanbekov@innoforce.kz>
2024-11-01 15:57:37 +0500

    feat: added btn to display more products

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import { fetchProducts } from './api/products.js';
 
+let allProducts = [];
+let displayedCount = 0;
+
 function displayProducts(products) {
 	const productContainer = document.createElement('div');
 	productContainer.className = 'product-container';
 
-	products.forEach(product => {
+	const limitedProducts = products.slice(displayedCount, displayedCount + 4);
+
+	limitedProducts.forEach(product => {
 		const productCard = document.createElement('div');
 		productCard.className = 'product-card';
 		productCard.innerHTML = `
@@ -17,15 +22,30 @@ function displayProducts(products) {
 	});
 
 	document.body.appendChild(productContainer);
+	displayedCount += limitedProducts.length;
+
+	if (displayedCount < products.length) {
+		document.getElementById('showMoreBtn').style.display = 'block';
+	} else {
+		document.getElementById('showMoreBtn').style.display = 'none';
+	}
 }
 
 const loadProducts = async () => {
 	try {
-		const products = await fetchProducts();
-		displayProducts(products);
+		allProducts = await fetchProducts();
+		displayProducts(allProducts);
 	} catch (error) {
 		console.error('Failed to load products:', error);
 	}
 };
 
+const showMoreProducts = () => {
+	displayProducts(allProducts);
+};
+
 loadProducts();
+
+document
+	.getElementById('showMoreBtn')
+	.addEventListener('click', showMoreProducts);
